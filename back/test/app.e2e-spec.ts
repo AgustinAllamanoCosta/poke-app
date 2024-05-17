055db257b38095364770817de7d87c99e6b86ac9
AgustinaAllamanoCosta <allamanocostaagustin@gmail.com>
2024-05-17 12:50:51 +1000

    fix: 🐛 e2e test import

--- back/test/app.e2e-spec.ts
+++ back/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { healthCheckResponse, systemDate } from './fixture/healthCheck';
 // eslint-disable-next-line
